a059b68c76236585371992e1d9ddb659d92515ea
danish alam <danish alam@LAPTOP-ARGGG25A>
2021-06-16 12:32:46 +0530

    new

---
+++ SDET_Fram_15_afreena/src/test/java/Win.java
@@ -0,0 +1,17 @@
+
+public class Win {
+	/*CreateConatctWithOrganization.java
+	 * Set<String> windows= driver.getWindowHandles();
+				
+				for(String window:windows) {
+					if(!window.equals(mainwindow)) {
+						driver.switchTo().window(window);
+					}
+				}
+			    driver.findElement(By.linkText("ty-390")).click();
+			    
+			    driver.switchTo().window(mainwindow);
+			    
+			    */
+
+}
